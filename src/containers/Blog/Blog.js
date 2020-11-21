3654bf5595ba5df1b4938a772914f83e9c0ae93b
shishir75 <shishir.srdr16@gmail.com>
2020-11-21 23:54:13 +0600

    Restructure Folder

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 // import axios from "axios";
 import axios from "../../axios";
 
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
+// import Post from "../../components/Post/Post";
+// import FullPost from "./FullPost/FullPost";
+// import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -73,12 +73,12 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <section className="Posts">{posts}</section>
-                <section>
+                {/* <section>
                     <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
-                </section>
+                </section> */}
             </div>
         );
     }
