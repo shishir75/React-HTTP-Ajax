1aee0bd31e7867aef0097ec191e4abd65c4aff62
shishir75 <shishir.srdr16@gmail.com>
2020-11-23 22:56:05 +0600

    Passing Route Parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import "./Blog.css";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 import { Route, NavLink } from "react-router-dom";
+import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
     render() {
@@ -35,6 +36,7 @@ class Blog extends Component {
 
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
 
                 {/* <section>
                     <FullPost id={this.state.selectedPostId} />
