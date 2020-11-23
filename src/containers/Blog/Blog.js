527435d2bf6f74d976f12df8641cd9cb54944e28
shishir75 <shishir.srdr16@gmail.com>
2020-11-23 23:35:23 +0600

    Using Switch to Load a Single Route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
@@ -34,9 +34,11 @@ class Blog extends Component {
 
                 {/* <Route path="/" exact render={() => <h1>Home Page</h1>} /> */}
 
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
 
                 {/* <section>
                     <FullPost id={this.state.selectedPostId} />
