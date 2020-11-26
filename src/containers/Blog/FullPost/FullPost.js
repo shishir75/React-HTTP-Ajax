20db87715e9af782ea452895c9dc0562d754cbeb
shishir75 <shishir.srdr16@gmail.com>
2020-11-27 00:19:32 +0600

    Creating Dynamic Nested Routes

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -10,11 +10,19 @@ class FullPost extends Component {
 
     componentDidMount() {
         // console.log(this.props);
+        this.loadData();
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+    }
+
+    loadData() {
         if (this.props.match.params.id) {
             if (
                 !this.state.loadedPost ||
                 (this.state.loadedPost &&
-                    this.state.loadedPost.id !== this.props.match.params.id)
+                    this.state.loadedPost.id != this.props.match.params.id)
             ) {
                 axios
                     .get("/posts/" + this.props.match.params.id)
